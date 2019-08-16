17794fcc1df9da9990fb1a21211b871011f07980
spalmer8891 <48135720+spalmer8891@users.noreply.github.com>
2019-08-16 14:49:21 -0400

    Update captchaValidate.js

--- scripts/captcha/captchaValidate.js
+++ scripts/captcha/captchaValidate.js
@@ -54,7 +54,7 @@ $(document).ready(function() {
                 if (username.toLowerCase() == key.toString().toLocaleLowerCase() && password == value && isUser == false) {
                     isUser = true;
                     if ($('ul').validate() && isUser == true) {
-                        window.location = "../../../views/SiteMaster.html";
+                        window.location = "JustCars/../views/SiteMaster.html";
                         break;
                     } else {
 
